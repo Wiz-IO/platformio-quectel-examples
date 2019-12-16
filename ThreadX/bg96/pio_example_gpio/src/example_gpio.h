edcd5ceaad55b835b07640b30d3b3f699286b4d9
Georgi Angelov <the.wizarda@gmail.com>
2019-12-16 09:37:24 +0200

    quectel to platformio

---
+++ ThreadX/bg96/pio_example_gpio/src/example_gpio.h
@@ -0,0 +1,21 @@
+/******************************************************************************
+*@file    example_gpio.h
+*@brief   example of gpio operation
+*
+*  ---------------------------------------------------------------------------
+*
+*  Copyright (c) 2018 Quectel Technologies, Inc.
+*  All Rights Reserved.
+*  Confidential and Proprietary - Quectel Technologies, Inc.
+*  ---------------------------------------------------------------------------
+*******************************************************************************/
+#ifndef __EXAMPLE_GPIO_H__
+#define __EXAMPLE_GPIO_H__
+
+#if defined(__EXAMPLE_GPIO__)
+/* Modify this which mode(input/output) you want to test */
+#define GPIO_TEST_MODE   0	//1: test pin output; 0: test pin input;
+
+#endif /*__EXAMPLE_GPIO__*/
+
+#endif /*__EXAMPLE_GPIO_H__*/
